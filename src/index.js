dc54e7593c9cfa1480f029d66c50320fef8ed4fa
Sijioth <jhnsmthmit@gmail.com>
2019-03-15 13:42:27 +0300

    master/feat: Add final solution.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  let stack = [];
-  for (let i = 0; i < str.length; i++){
-    stack.push(str[i]);
-    // if (str[i + 1] == bracketsConfig[stack[stack.length - 1]]) stack.pop();
-    // console.log("closing bracket form config:" + bracketsConfig[stack[stack.length - 1]]);
-    // console.log(stack[stack.length - 1]);
-    for (let j = 0; j < bracketsConfig.length; j++) {
-      // let arr = bracketsConfig[j];
-      // console.log(arr[stack[stack.length - 1]]);
-
-      for (let key in bracketsConfig[j]) {
-        // console.log("key: " + key + " value: " + bracketsConfig[j][1]);
-      // console.log(stack[stack.length - 1] in bracketsConfig);
-      // console.log(bracketsConfig[stack[stack.length - 1]]);
-      if (str[i + 1] == bracketsConfig[j][stack[stack.length - 1]]) stack.pop();
-      }
-    }
-    // for (const key in bracketsConfig) {
-    //   console.log(stack[stack.length - 1] in bracketsConfig);
-    // }
-    // while (str[i + 1] == bracketsConfig[stack[stack.length - 1]]) {
-    //   stack.pop();
-    //   i++;
-    //   if (i >= str.length) break;
-    // }
+  // my solution
+  let obj = {};                                       // convert bracketsConfig to associated array (object)
+  for (let k = 0; k < bracketsConfig.length; k++) {
+    obj[bracketsConfig[k][0]] = bracketsConfig[k][1];
   }
-  // console.log(stack);
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++){               // treat input string like stack
+    stack.push(str[i]);
+      while (str[i + 1] == obj[stack[stack.length - 1]]) {  // while next value is closing brackets, we'll take values from the stack one by one
+        stack.pop();
+        i++;
+        if (str[i + 1] === undefined) break;
+      }
+  }
+  
   return stack.length == 0 ? true : false;
 }
